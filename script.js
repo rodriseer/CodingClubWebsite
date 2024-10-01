bd5631c924975847d75a01cd61cb629a81faea16
Rodrigo <rodrigo16seer@gmail.com>
2024-10-01 17:29:24 -0400

    Commit for new update 0.5

--- script.js
+++ script.js
@@ -4,3 +4,37 @@ function toggleSection(id) {
     var section = document.getElementById(id);
     section.style.display = (section.style.display === 'none' || section.style.display === '') ? 'block' : 'none';
 }
+
+
+// immutable string in js
+const letters = "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
+// script to make words effects on appear disappear with anything but I chose numbers idk
+// snippet of when user hovers over the words on the website it will trigger an action, decalres the css code inside the query selector
+document.querySelector(".club-subtitle").onmouseover = event =>{
+    // making sure the letters dont change when not neccessary
+    let iterations = 0;
+
+    // this snippet of code will fetch the number of letter that you want to iterate
+    const targetTextLength = event.target.dataset.value.length;
+
+
+    // runner to make sure the letters changes on their own
+    const interval = setInterval(() => {
+
+    // split letters so each one is random uniquely
+        event.target.innerText = event.target.innerText.split("")
+            // event maker, randoom number between 0 and 26 and random letters from const letters string
+                // maps to each letter in the total word
+                .map((letter, index) => {
+                    if(index < iterations) {
+                        return event.target.dataset.value[index]
+                    }
+                    return letters[Math.floor(Math.random() * 26)]
+                })
+                .join("");
+
+            // if iterations reach 9 then clear the interval and reset the counter
+            if(iterations >= targetTextLength) clearInterval(interval);
+            iterations +=1/2;
+    }, 30)
+}
