d11f69c9276714341385eb3bc1d5ceea7db75f88
Rodrigo <rodrigo16seer@gmail.com>
2024-10-01 16:09:12 -0400

    Website update 0.4

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// js script for dropdown effects
+function toggleSection(id) {
+    var section = document.getElementById(id);
+    section.style.display = (section.style.display === 'none' || section.style.display === '') ? 'block' : 'none';
+}
